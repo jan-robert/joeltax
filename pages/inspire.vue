3a0e960924d9cbe84c624a12a7db64c859ce70ac
jan-robert <janrobertyeaton@gmail.com>
2021-10-19 15:28:23 -0500

    test push

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -7,10 +7,10 @@
         class="mb-5"
       >
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;Deez nuts.&#8221;
         <footer>
           <small>
-            <em>&mdash;John Johnson</em>
+            <em>&mdash;Juni Yeaton</em>
           </small>
         </footer>
       </blockquote>
